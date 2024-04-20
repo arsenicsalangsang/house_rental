c8e124fbee49d3970575b391fb976c1d2fef5ca6
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-20 20:17:22 +0800

    homepage

--- homepage.php
+++ homepage.php
@@ -86,7 +86,6 @@
   }
 
   /* ===== Home ===== */
-
   .wrapper{
     display: flex;
     align-items: center;
@@ -125,6 +124,8 @@
     margin-right: 20px;
   }
 
+  /* ===== Preview ===== */
+
   #preview{
     margin-bottom: 75px;
   }
@@ -138,11 +139,200 @@
     text-align: center;
   }
 
-  #title3{
+  /* ===== Quick Stats ===== */
+
+  #title3, #title4, #descr, #title5{
     font-family: 'Roboto', sans-serif;
     text-align: center;
   }
 
+  .quickstats{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    margin-top: 50px;
+    margin-left: 170px;
+  }
+
+  .box {
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  padding: 10px;
+  width: 260px;
+  font-weight: 300;
+  border-right: 10px;
+  border-radius: 10px;
+  }
+
+  .class{
+    padding: 40px;
+    font-weight: 300;
+    border-right: 10px;
+    border-radius: 10px;
+  }
+
+  #icon {
+    width: 60px;
+    height: 60px;
+  }
+
+  #title4 {
+    margin-bottom: 5px;
+  }
+
+  /* ===== Add New Apartment ===== */
+
+  .wrapper1{
+    align-items: center;
+    background-color: #E1D5C9;
+    width: 1519px;
+    height:480px; 
+    margin-top: 20px;
+    position: relative;
+  }
+
+  #title5{
+    position: absolute;
+    top: 50px; 
+    left: 50%; 
+    transform: translateX(-50%); 
+  }
+
+  .input-form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px; 
+    position: relative; 
+    top: 110px; 
+  }
+
+  .custom-input{
+    padding: 10px;
+    margin-bottom: 2px; 
+    border: none; 
+    border-radius: 10px;
+    background-color: white; 
+    outline: none; 
+    width: 500px;
+  }
+
+  .title-input{
+    font-family: 'Roboto', sans-serif;
+    margin-bottom: 2px;
+    margin-left: 5px;
+  }
+
+  /* ===== About Us ===== */
+  .wrapper2{
+    align-items: center;
+    background-color: #E1D5C9;
+    width: 1519px;
+    height:600px; 
+    margin-top: 20px;
+    position: relative;
+  }
+
+  #des{
+    position: absolute;
+    top: 90px; 
+    left: 50%; 
+    transform: translateX(-50%); 
+    font-family: 'Roboto', sans-serif;
+  }
+
+  #tuppic{
+    position: absolute;
+    width: 600px;
+    height: 364px;
+    top: 200px; 
+    left: 100px; 
+    border: 4px solid white;
+    border-radius: 10px;
+  }
+
+   #fourpic{
+    position: absolute;
+    width: 600px;
+    top: 200px; 
+    left: 800px; 
+    border: 4px solid white;
+    border-radius: 10px;
+  }
+
+  /* ===== Objective ===== */
+  #title6{
+    font-family: 'Roboto', sans-serif;
+    margin-left: 100px;
+    margin-top: 50px;
+  }
+
+  #ovm{
+    font-family: 'Roboto', sans-serif;
+    margin-left: 100px;
+  }
+
+  /* ===== Contact ===== */
+  .wrapper3{
+    align-items: center;
+    background-color: #E1D5C9;
+    width: 1519px;
+    height:380px; 
+    margin-top: 50px;
+    position: relative;
+  }
+
+  #title7{
+    font-family: 'Roboto', sans-serif;
+    position: absolute;
+    top: 50px;
+    left: 100px;
+  }
+
+  #contact-des{
+    font-family: 'Roboto', sans-serif;
+    position: absolute;
+    top: 100px;
+    left: 100px;
+  }
+
+  #icon1{
+    width: 48px;
+    height: 48px;
+    position: absolute;
+    left: 100px;
+    top: 180px;
+  }
+
+  #icon2{
+    width: 48px;
+    height: 48px;
+    position: absolute;
+    left: 100px;
+    top: 250px;
+  }
+
+  #fb{
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    position: absolute;
+    left: 170px;
+    top: 170px;
+  }
+
+  #google{
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    position: absolute;
+    left: 170px;
+    top: 240px;
+  }
+
+  footer{
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    font-size: 15px;
+  }
 
 </style>
 
@@ -169,7 +359,7 @@
       <div class = "content">
         <h1 id = "title"> Your Apartment Management System </h1>
         <p id = "description"> See your apartments in a better, organized, and modern perspective</p>
-        <a onclick = "window.location.href = 'login.php'" id = "button-start"> Get Started</a>
+        <a onclick = "window.location.href = 'signup.php'" id = "button-start"> Get Started</a>
       </div>
       <img src = "assets\pictures\homepagepic.png" id = hppic>
     </div>
@@ -183,12 +373,95 @@
   <!-- ===== Quick Stats ===== -->
   <section id = "stats">
     <h2 id = "title3"> Quick Stats </h2>
-    <div id = "quickstats">
-      
+    <div class = "quickstats">
+      <div class = "box">
+          <img src = "assets\pictures\total.png" id = icon>
+          <h4 id = "title4"> Total Apartments </h4>
+          <h3 id = "descr"> 9 </h4>
+      </div>
+      <div class = "box">
+          <img src = "assets\pictures\available.png" id = icon>
+          <h4 id = "title4"> Available Apartments </h4>
+          <h3 id = "descr"> 3 </h4>
+      </div>
+      <div class = "box">
+          <img src = "assets\pictures\pending.png" id = icon>
+          <h4 id = "title4"> Pending Payments </h4>
+          <h3 id = "descr"> 2 </h4>
+      </div>
     </div>
-
   </section>
 
+  <!-- ===== Add Apartment ===== -->
+  <section id = "add">
+    <div class = "wrapper1">
+      <div class = "content1">
+        <h2 id = "title5"> Add New Apartment </h2>
+        <div class = "input-form">
+          <div>
+            <h4 class = "title-input">Category</h4>
+            <input type="text" name="Category" placeholder="Create or select Category" class="custom-input">
+          </div>
+          <div>
+            <h4 class = "title-input">Apartment Name</h4>
+            <input type="text" name="ApartmentName" placeholder="Enter Apartment Name" class="custom-input">
+          </div>
+          <div>
+            <h4 class = "title-input">Rent Rate</h4>
+            <input type="text" name="RentRate" placeholder="Rent Rate" class="custom-input">
+          </div>
+          <div>
+            <h4 class = "title-input">Contract Length</h4>
+            <input type="text" name="ContractLength" placeholder="Contract Length" class="custom-input">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <!-- ===== Data ===== -->
+  <section id = "data">
+    <h2 id = "title3"> Data Visualized </h2>
+  </section>
+
+  <!-- ===== About ===== -->
+  <section id = "about">
+    <h1 id = "title2"> About Us </h1>
+      <div class = "wrapper2">
+        <div class = "content1">
+          <h2 id = "title5"> We are LivWell </h2>
+          <h4 id = "des"> LivWell is a web-based apartment management system. It allows landlords to manage their properties. Additionally, by using LivWell, it improves data organization, enhances efficiency, and makes it convenient for landlords and renters. </h4>
+        </div>
+        <img src = "assets\pictures\four.jpg" id = "fourpic">
+        <img src = "assets\pictures\tup.png" id = "tuppic">
+      </div>
+  </section>
+
+  <!-- ===== Objective ===== -->
+  <section id = "objective">
+    <h2 id = "title6"> Our Objective </h2>
+    <h4 id = "ovm" >To provide an intuitive, web-based apartment management system that streamlines property management tasks, enhances data organization, and fosters seamless communication between landlords and renters.</h4>
+    <h2 id = "title6"> Our Vision </h2>
+    <h4 id = "ovm" >To become the leading platform for apartment management, recognized for our commitment to simplifying property management processes, optimizing data utilization, and fostering positive relationships between landlords and renters. We envision a future where LivWell transforms the rental industry, making property management effortless and renting a joyous experience for all parties involved.</h4>
+    <h2 id = "title6"> Our Mission</h2>
+    <h4 id = "ovm" >Our mission is to empower landlords with the tools they need to efficiently manage their properties while offering renters a convenient and transparent renting experience. Through continuous innovation and customer-centric approaches, we aim to revolutionize the way rental properties are managed and experienced.</h4>
+  </section>
+
+  <!-- ===== Contact Us ===== -->
+  <section id = "contact">
+    <div class = "wrapper3">
+      <h2 id = "title7">Contact Us</h2>
+      <h4 id = "contact-des"> Let us know what you think through the channels below: </h4>
+      <img src = "assets\pictures\fb.png" id = "icon1">
+      <img src = "assets\pictures\google.png" id = "icon2">
+      <p id = "fb"> LivWell </p>
+      <p id = "google"> livwell@gmail.com </p>
+    </div>
+  </section>
+
+  <!-- ===== Footer ===== -->
+  <footer>
+    <p>Copyright &#169; 2024 LivWell: Apartment Management System. All Rights Reserved.</p>
+  </footer>
 </body>
 </html>
