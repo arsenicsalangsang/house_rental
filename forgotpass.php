0e444537d6aad91ad4c5b4dcda8a16eed4806926
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-21 14:32:37 +0800

    forgotpass.php

--- forgotpass.php
+++ forgotpass.php
@@ -19,181 +19,12 @@
 
   <title><?php echo $_SESSION['system']['name'] = "LivWell"; ?></title>
   <link rel="icon" type="image/png" href="assets\pictures\livwellLogo.png">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@0,400;0,700;0,800;0,900;1,400;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <?php include('./header.php'); ?>
+  <link rel="stylesheet" href="assets\css\newstyle.css">
   <?php 
     if(isset($_SESSION['login_id']))
     header("location:index.php?page=home");
     ?>
 </head>
-
-<style>
-	body{
-	margin: 0;
-	padding: 0;
-	}
-
-	/* ===== Navigation ===== */
-	#desktop-nav {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	margin-top: 10px;
-	}
-
-	#logoimg{
-	width: 50px;
-	margin-left: 30px;
-	margin-right: 10px;
-	}
-
-	.logoText {
-	font-family: "Sansita", sans-serif;
-	font-style: italic;
-	font-size: 25px;
-	font-weight: bold;
-	color: #c59452;
-	margin-right: 20px;
-	}
-
-	#nav-links {
-	display: flex;
-	justify-content: flex-end;
-	flex-grow: 1; 
-	margin-right: 45px;
-	}
-
-	#nav-links ul {
-	margin: 0; 
-	padding: 0; 
-	}
-
-	#nav-links li {
-	list-style: none;
-	margin-left: 20px; 
-	}
-
-	#nav-links li a {
-	font-family: 'Roboto', sans-serif;
-	color: black;
-	text-decoration: none;
-	font-size: 16px;
-	position: relative;
-	}
-
-	#nav-links li a::after {
-	content: "";
-	width: 0;
-	height: 3px;
-	background: #E1D5C9;
-	position: absolute;
-	left: 0;
-	bottom: -6px;
-	transition: 0.5s;
-	}
-
-	#nav-links li a:hover {
-	color: #c59452;
-	}
-
-	#nav-links li a:hover::after {
-	width: 100%;
-	}
-
-	/* ===== Main ===== */
-	.wrapper{
-		align-items: center;
-		background-color: #E1D5C9;
-		width: 1519px;
-		height:432px; 
-		margin-top: 50px;
-		position: relative;
-	}
-
-	#title1{
-		font-family: 'Roboto', sans-serif;
-		position: absolute;
-		top: 75px;
-		left: 50%;
-		transform: translateX(-50%);
-		color: black;
-		font-size: 40px;
-	}
-
-	#description{
-		position: absolute;
-		top: 130px; 
-		left: 50%; 
-		transform: translateX(-50%); 
-		font-family: 'Roboto', sans-serif;
-		color: black;
-		font-size: 16px;
-	}
-	
-	#input-form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px; 
-        position: relative; 
-        top: 200px; 
-	}
-
-	.custom-input{
-        padding: 10px;
-        margin-bottom: 8px; 
-        border: none; 
-        border-radius: 10px;
-        background-color: white; 
-        outline: none; 
-        width: 600px;
-	}
-
-	.title-input{
-        font-family: 'Roboto', sans-serif;
-        margin-bottom: 2px;
-        margin-left: 5px;
-        font-size: 14px;
-        color: black;
-	}
-
-    #loginbtn{
-        background-color: #E1D5C9;
-        color: black;
-        border: 2px solid black;
-        border-radius: 10px;
-        padding: 10px;   
-    }
-
-    #submitbtn{
-        background-color: #c59452;
-        color: white;
-        border-radius: 10px;
-        border: 2px solid #c59452;
-        padding: 10px;
-    }
-    
-    .button-container {
-        margin-top: 15px;
-        text-align: center; 
-    }
-
-    #loginbtn, #submitbtn {
-        display: inline-block;
-        width: 234px; 
-        margin: 0 10px; 
-    }
-
-    footer{
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-    font-size: 15px;
-    }
-
-</style>
 
 <body>
   <!-- ===== Navigation ===== -->
@@ -211,16 +42,16 @@
 
   <!-- ===== Main ===== -->
   <section id = "main">
-	<div class = "wrapper">
-		<h2 id = "title1"> Forgot Password </h2>
-		<p id = "description"> Enter your email to recover </p>
-		<form id = "input-form">
+	<div class = "wrapper-forgot">
+		<h2 id = "title1Main"> Forgot Password </h2>
+		<p id = "descriptionMain"> Enter your email to recover </p>
+		<form id = "input-form-forgot">
 			<div>
-				<h4 class = "title-input">Email</h4>
-            	<input type="text" name="username" placeholder="Enter your email" class="custom-input">
+				<h4 class = "title-inputMain">Email</h4>
+            	<input type="text" name="username" placeholder="Enter your email" class="custom-inputMain">
 			</div>
             <div class="button-container">
-                <button type="button" id="loginbtn">Existing User? Login Here</button>
+                <button type="button" id="loginbtn-signup">Existing User? Login Here</button>
                 <button type="submit" id="submitbtn">Submit</button>
             </div>
         </form>
@@ -237,7 +68,7 @@
 <script>
 	$(document).ready(function() {
 
-		$('#loginbtn').click(function() {
+		$('#loginbtn-signup').click(function() {
                 window.location.href = 'login.php';
         });
 	});
