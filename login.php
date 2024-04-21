8013536d95d6659dd0c970807ea6b62396364590
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-21 13:50:39 +0800

    login.php

--- login.php
+++ login.php
@@ -19,202 +19,12 @@
 
   <title><?php echo $_SESSION['system']['name'] = "LivWell"; ?></title>
   <link rel="icon" type="image/png" href="assets\pictures\livwellLogo.png">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@0,400;0,700;0,800;0,900;1,400;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <?php include('./header.php'); ?>
+  <link rel="stylesheet" href="assets\css\newstyle.css">
   <?php 
     if(isset($_SESSION['login_id']))
     header("location:index.php?page=home");
     ?>
 </head>
-
-<style>
-	body{
-	margin: 0;
-	padding: 0;
-	}
-
-	/* ===== Navigation ===== */
-	#desktop-nav {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	margin-top: 10px;
-	}
-
-	#logoimg{
-	width: 50px;
-	margin-left: 30px;
-	margin-right: 10px;
-	}
-
-	.logoText {
-	font-family: "Sansita", sans-serif;
-	font-style: italic;
-	font-size: 25px;
-	font-weight: bold;
-	color: #c59452;
-	margin-right: 20px;
-	}
-
-	#nav-links {
-	display: flex;
-	justify-content: flex-end;
-	flex-grow: 1; 
-	margin-right: 45px;
-	}
-
-	#nav-links ul {
-	margin: 0; 
-	padding: 0; 
-	}
-
-	#nav-links li {
-	list-style: none;
-	margin-left: 20px; 
-	}
-
-	#nav-links li a {
-	font-family: 'Roboto', sans-serif;
-	color: black;
-	text-decoration: none;
-	font-size: 16px;
-	position: relative;
-	}
-
-	#nav-links li a::after {
-	content: "";
-	width: 0;
-	height: 3px;
-	background: #E1D5C9;
-	position: absolute;
-	left: 0;
-	bottom: -6px;
-	transition: 0.5s;
-	}
-
-	#nav-links li a:hover {
-	color: #c59452;
-	}
-
-	#nav-links li a:hover::after {
-	width: 100%;
-	}
-
-	/* ===== Main ===== */
-	#title{
-		font-family: "Sansita", sans-serif;
-		font-style: bold;
-		font-size: 48px;
-		font-weight: 400px;
-		color: #c59452;
-		text-align: center;
-		margin-top: 5px;
-	}
-
-	.wrapper{
-		align-items: center;
-		background-color: #E1D5C9;
-		width: 1519px;
-		height:480px; 
-		margin-top: 50px;
-		position: relative;
-	}
-
-	#title1{
-		font-family: 'Roboto', sans-serif;
-		position: absolute;
-		top: 50px;
-		left: 50%;
-		transform: translateX(-50%);
-		color: black;
-		font-size: 40px;
-	}
-
-	#description{
-		position: absolute;
-		top: 105px; 
-		left: 50%; 
-		transform: translateX(-50%); 
-		font-family: 'Roboto', sans-serif;
-		color: black;
-		font-size: 16px;
-	}
-	
-	#input-form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px; 
-        position: relative; 
-        top: 150px; 
-	}
-
-	.custom-input{
-        padding: 10px;
-        margin-bottom: 8px; 
-        border: none; 
-        border-radius: 10px;
-        background-color: white; 
-        outline: none; 
-        width: 600px;
-	}
-
-	.title-input{
-        font-family: 'Roboto', sans-serif;
-        margin-bottom: 2px;
-        margin-left: 5px;
-        font-size: 14px;
-        color: black;
-	}
-
-    #loginbtn{
-	    background-color: #c59452;
-        color: white;
-        border-radius: 10px;
-        border: 2px solid #c59452;
-        padding: 10px;
-    }
-
-    #signupbtn{
-		background-color: #E1D5C9;
-        color: black;
-        border: 2px solid black;
-        border-radius: 10px;
-        padding: 10px; 
-    }
-    
-    #fpassbtn{
-        background-color: black;
-        color: white;
-        padding: 10px;
-        border-radius: 10px;
-        width: 480px;
-        position: relative;
-        top: 15px;
-    }
-
-    .button-container {
-        margin-top: 15px;
-        text-align: center; 
-    }
-
-    #signupbtn, #loginbtn {
-        display: inline-block;
-        width: 234px; 
-        margin: 0 10px; 
-    }
-
-    footer{
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-    font-size: 15px;
-    }
-
-</style>
-
 <body>
   <!-- ===== Navigation ===== -->
   <section id = "header">
@@ -231,22 +41,22 @@
 
   <!-- ===== Main ===== -->
   <section id = "main">
-	<h1 id = "title"> Join us with an Account </h1>
-	<div class = "wrapper">
-		<h2 id = "title1"> Login </h2>
-		<p id = "description"> Sign in to your account </p>
-		<form id = "input-form">
+	<h1 id = "titleMain"> Join us with an Account </h1>
+	<div class = "wrapperMain">
+		<h2 id = "title1Main"> Login </h2>
+		<p id = "descriptionMain"> Sign in to your account </p>
+		<form id = "input-formMain">
 			<div>
-				<h4 class = "title-input">Username</h4>
-            	<input type="text" name="username" placeholder="Enter your username" class="custom-input">
+				<h4 class = "title-inputMain">Username</h4>
+            	<input type="text" name="username" placeholder="Enter your username" class="custom-inputMain">
 			</div>
 			<div>
-				<h4 class = "title-input">Password</h4>
-            	<input type="password" name="password" placeholder="Enter your password" class="custom-input">
+				<h4 class = "title-inputMain">Password</h4>
+            	<input type="password" name="password" placeholder="Enter your password" class="custom-inputMain">
 			</div>
             <div class="button-container">
-                <button type="button" id="signupbtn">New User? Create Account</button>
-                <button type="submit" id="loginbtn">Login</button>
+                <button type="button" id="signupbtnMain">New User? Create Account</button>
+                <button type="submit" id="loginbtnMain">Login</button>
             </div>
             <button id = "fpassbtn">Forgot Password?</button>
         </form>
@@ -262,7 +72,7 @@
 <script>
 	$(document).ready(function() {
 
-		$('#signupbtn').click(function() {
+		$('#signupbtnMain').click(function() {
                 window.location.href = 'signup.php';
         });
 
@@ -270,9 +80,9 @@
                 window.location.href = 'forgotpass.php';
         });
 
-		$('#input-form').submit(function(e) {
+		$('#input-formMain').submit(function(e) {
 			e.preventDefault();
-			$('#loginbtn').attr('disabled', true).html('Logging in...');
+			$('#loginbtnMain').attr('disabled', true).html('Logging in...');
 			if ($(this).find('.alert-danger').length > 0)
 				$(this).find('.alert-danger').remove();
 			$.ajax({
@@ -281,14 +91,14 @@
 				data: $(this).serialize(),
 				error: function(err) {
 					console.log(err);
-					$('#loginbtn').removeAttr('disabled').html('Login');
+					$('#loginbtnMain').removeAttr('disabled').html('Login');
 				},
 				success: function(resp) {
 					if (resp == 1) {
 						location.href = 'index.php?page=home';
 					} else {
-						$('#input-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>');
-						$('#loginbtn').removeAttr('disabled').html('Login');
+						$('#input-formMain').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>');
+						$('#loginbtnMain').removeAttr('disabled').html('Login');
 					}
 				}
 			});
