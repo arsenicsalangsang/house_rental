f3049fd0ec5e3564ad96c9f3138d527020ebea46
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-22 23:35:54 +0800

    home.php

--- home.php
+++ home.php
@@ -2,7 +2,29 @@
 
 <div class = "wrapper">
     <div class = "content">
-        <h1 id = "title"> Your Apartment Management System </h1>
-        <p id = "description"> See your apartments in a better, organized, and modern perspective</p>
+        <h1 id = "title"> Welcome, <?php echo $_SESSION['login_name']."!"  ?> </h1>
+        <p id = "description"> See the summary of your apartments! </p>
+    </div>
+    <img src = "assets\pictures\homepagepic.png" id = hppic>
+</div>
+
+<div id = "stats">
+    <h2 id = "title3"> Quick Stats </h2>
+    <div class = "quickstats">
+        <div class = "box">
+            <img src = "assets\pictures\total.png" id = icon>
+            <h4 id = "title4"> Total Apartments </h4>
+            <h3 id = "descr"> 9 </h4>
+        </div>
+        <div class = "box">
+            <img src = "assets\pictures\available.png" id = icon>
+            <h4 id = "title4"> Available Apartments </h4>
+            <h3 id = "descr"> 3 </h4>
+        </div>
+        <div class = "box">
+            <img src = "assets\pictures\pending.png" id = icon>
+            <h4 id = "title4"> Pending Payments </h4>
+            <h3 id = "descr"> 2 </h4>
+        </div>
     </div>
 </div>
