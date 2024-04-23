5e4c396eb7e9a4f238bd2b18d4c3851b431f0181
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-23 08:46:06 +0800

    home.php

--- home.php
+++ home.php
@@ -12,19 +12,37 @@
     <h2 id = "title3"> Quick Stats </h2>
     <div class = "quickstats">
         <div class = "box">
-            <img src = "assets\pictures\total.png" id = icon>
+            <img src = "assets\pictures\total.png" id = "icon">
             <h4 id = "title4"> Total Apartments </h4>
-            <h3 id = "descr"> 9 </h4>
+            <h3 id = "descr"> <?php echo $conn->query("SELECT * FROM houses")->num_rows ?> </h3>
         </div>
         <div class = "box">
-            <img src = "assets\pictures\available.png" id = icon>
+            <img src = "assets\pictures\available.png" id = "icon">
             <h4 id = "title4"> Available Apartments </h4>
-            <h3 id = "descr"> 3 </h4>
+            <h3 id = "descr"> 3 </h3>
         </div>
         <div class = "box">
-            <img src = "assets\pictures\pending.png" id = icon>
+            <img src = "assets\pictures\pending.png" id = "icon">
             <h4 id = "title4"> Pending Payments </h4>
-            <h3 id = "descr"> 2 </h4>
+            <h3 id = "descr"> 2 </h3>
         </div>
+    </div>
+</div>
+
+<div class = "data-card">
+    <div class = "card-data">
+        <p class = "title-card">Occupany Rate</p>
+        <p class = "data-analytic"> data </p>
+        <p class = "data-analytic-2"> data </p>
+    </div>
+    <div class = "card-data">
+        <p class = "title-card">Last Month's Totals</p>
+        <p class = "data-analytic"> data </p>
+        <p class = "data-analytic-2"> data </p>
+    </div>
+    <div class = "card-data">
+        <p class = "title-card">Previous Six (6) Month's Totals</p>
+        <p class = "data-analytic"> data </p>
+        <p class = "data-analytic-2"> data </p>
     </div>
 </div>
