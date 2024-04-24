1539660af933d9ea991254ed0babb4dce89310a6
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-24 11:54:45 +0800

    tenants.php
    earnings

--- tenants.php
+++ tenants.php
@@ -64,3 +64,13 @@
 		</div>
 	</div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+	$(document).ready(function() {
+
+		$('#transactionbtn').click(function() {
+                window.location.href = 'earningtransaction.php';
+        });
+	});
+</script>
