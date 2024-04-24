b3b41df3ca35dce9b4841cafc3fe92e007ee1800
jewelvaida <jewelvaidaajaime@gmail.com>
2024-04-24 20:23:23 +0800

    manageapartment

--- categories.php
+++ categories.php
@@ -1,143 +1,84 @@
 <?php include('db_connect.php');?>
 
-<div class="container-fluid">
-	
-	<div class="col-lg-12">
-		<div class="row">
-			<!-- FORM Panel -->
-			<div class="col-md-4">
-			<form action="" id="manage-category">
-				<div class="card">
-					<div class="card-header">
-						    Category Form
-				  	</div>
-					<div class="card-body">
-							<input type="hidden" name="id">
-							<div class="form-group">
-								<label class="control-label">Name</label>
-								<input type="text" class="form-control" name="name">
-							</div>
-					</div>
-							
-					<div class="card-footer">
-						<div class="row">
-							<div class="col-md-12">
-								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
-								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="$('#manage-category').get(0).reset()"> Cancel</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</form>
-			</div>
-			<!-- FORM Panel -->
-
-			<!-- Table Panel -->
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">
-						<b>Category List</b>
-					</div>
-					<div class="card-body">
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th class="text-center">#</th>
-									<th class="text-center">Category</th>
-									<th class="text-center">Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php 
-								$i = 1;
-								$category = $conn->query("SELECT * FROM categories order by id asc");
-								while($row=$category->fetch_assoc()):
-								?>
-								<tr>
-									<td class="text-center"><?php echo $i++ ?></td>
-									<td class="">
-										<p><b><?php echo $row['name'] ?></b></p>
-									</td>
-									<td class="text-center">
-										<button class="btn btn-sm btn-primary edit_category" type="button" data-id="<?php echo $row['id'] ?>"  data-name="<?php echo $row['name'] ?>" >Edit</button>
-										<button class="btn btn-sm btn-danger delete_category" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
-									</td>
-								</tr>
-								<?php endwhile; ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-			<!-- Table Panel -->
-		</div>
-	</div>	
-
+<div class = "apartment-banner">
+	<div class = "apartment-content">
+		<div class = "profilepic"></div>
+		<div class = "account-name"><?php echo $_SESSION['login_name']?></div>
+		<div class = "account-status">Landlord</div>
+	</div>
 </div>
-<style>
+
+<div class="col-md-8 table-style">
+	<div class="card">
+		<div class="card-header">
+			<b>House List</b>
+		</div>
+		<div class="card-body">
+			<table class="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th class="text-center">#</th>
+						<th class="text-center">House</th>
+						<th class="text-center">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+					$house = $conn->query("SELECT h.*,c.name as cname FROM houses h inner join categories c on c.id = h.category_id order by id asc");
+					while($row=$house->fetch_assoc()):
+					?>
+					<tr>
+						<td class="text-center"><?php echo $i++ ?></td>
+						<td class="">
+							<p>House #: <b><?php echo $row['house_no'] ?></b></p>
+							<p><small>House Type: <b><?php echo $row['cname'] ?></b></small></p>
+							<p><small>Description: <b><?php echo $row['description'] ?></b></small></p>
+							<p><small>Price: <b><?php echo number_format($row['price'],2) ?></b></small></p>
+						</td>
+						<td class="text-center">
+							<button class="edit_house" type="button" data-id="<?php echo $row['id'] ?>"  data-house_no="<?php echo $row['house_no'] ?>" data-description="<?php echo $row['description'] ?>" data-category_id="<?php echo $row['category_id'] ?>" data-price="<?php echo $row['price'] ?>" >Edit</button>
+							<button class="delete_house" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+						</td>
+					</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<div class = "wrapper-apartment">
+	<div class = "content1">
+		<h2 id = "title-apartment"> Add New Apartment </h2>
 	
-	td{
-		vertical-align: middle !important;
-	}
-</style>
-<script>
-	
-	$('#manage-category').submit(function(e){
-		e.preventDefault()
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=save_category',
-			data: new FormData($(this)[0]),
-		    cache: false,
-		    contentType: false,
-		    processData: false,
-		    method: 'POST',
-		    type: 'POST',
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully added",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
+		<form class = "input-form-apartment">
 
-				}
-				else if(resp==2){
-					alert_toast("Data successfully updated",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
+			<div>
+				<h4 class = "title-input-apartment">Lot</h4>
+				<input type="text" name="Category" placeholder="Create or select lot" class="custom-input-apartment">
+			</div>
+			<div>
+				<h4 class = "title-input-apartment">Apartment Name</h4>
+				<input type="text" name="ApartmentName" placeholder="Enter Apartment Name" class="custom-input-apartment">
+			</div>
+			<div>
+				<h4 class = "title-input-apartment">Rent Rate</h4>
+				<input type="text" name="RentRate" placeholder="Rent Rate" class="custom-input-apartment">
+			</div>
+			<div>
+				<h4 class = "title-input-apartment">Optional: Size </h4>
+				<input type="text" name="apartmentsize" placeholder="Enter size in sq. meters" class="custom-input-apartment">
+			</div>
+			<div>
+				<h4 class = "title-input-apartment">Address </h4>
+				<input type="text" name="apartmentaddress" placeholder="Enter address of apartment" class="custom-input-apartment">
+			</div>
+			<button type="submit" id="addapartmentbtn">Add</button>
+		</form>
+	</div>
+</div>
 
-				}
-			}
-		})
-	})
-	$('.edit_category').click(function(){
-		start_load()
-		var cat = $('#manage-category')
-		cat.get(0).reset()
-		cat.find("[name='id']").val($(this).attr('data-id'))
-		cat.find("[name='name']").val($(this).attr('data-name'))
-		end_load()
-	})
-	$('.delete_category').click(function(){
-		_conf("Are you sure to delete this category?","delete_category",[$(this).attr('data-id')])
-	})
-	function delete_category($id){
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=delete_category',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
-
-				}
-			}
-		})
-	}
-	$('table').dataTable()
-</script>
+<div class = "summary-data">
+	<p class = "summary-title">Summary</p>
+</div>
